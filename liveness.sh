fb71bdb6f568446527f44cf7680d5478ede6bc30
Akinlolu Akindele <AAkindele@users.noreply.github.com>
2022-08-07 22:36:37 -0500

    Sample liveness and readiness scripts (#18)
    * start on liveness script
    
    * add new file and rename another
    
    * added scripts for liveness and readiness

---
+++ liveness.sh
@@ -0,0 +1,41 @@
+set -e
+
+# how long the file can go without being updated
+threshold_seconds="$1"
+if [ -z "$threshold_seconds" ]
+then
+  echo "threshold_seconds is missing"
+  exit 1
+fi
+echo "threshold_seconds - $threshold_seconds"
+
+# file used to check of activity
+health_file="$2"
+if [ -z "$health_file" ]
+then
+  echo "health_file is missing"
+  exit 1
+fi
+echo "health_file - $health_file"
+
+# current time
+current_time=`date +%s`
+echo "current_time - $current_time"
+
+# beginning of time range we're checking
+cutoff_time=$(($current_time - $threshold_seconds))
+echo "cutoff_time - $cutoff_time"
+
+# last time file was updated
+last_update=`stat $health_file --format "%Y"`
+echo "last_update - $last_update"
+
+# last update to file is before the beginning of the required time range
+if [ $last_update -lt $cutoff_time ]
+then
+  echo "File has not been updated within threshold."
+  exit 1
+fi
+
+# last update to file is within the required time range
+echo "File has been updated within threshold."
