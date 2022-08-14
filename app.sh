2d4e0e775fe7c128f68a59df67e0e50d4b9c5bba
ak <a.akindele3000@gmail.com>
2022-08-13 21:41:42 -0500

    add option to delay start of sample app script

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# how long to sleep between file updates
+# how long to wait between file updates
 sleep_seconds=$1
 if [ -z "$sleep_seconds" ]
 then
@@ -8,6 +8,13 @@ then
   exit 1
 fi
 echo "sleep_seconds - $sleep_seconds"
+
+# start up wait time. default to 1 second if missing
+start_delay_seconds="${2:-1}"
+echo "start_delay_seconds - $start_delay_seconds"
+
+echo "sleeping for $start_delay_seconds seconds"
+sleep $start_delay_seconds
 
 while true
 do
