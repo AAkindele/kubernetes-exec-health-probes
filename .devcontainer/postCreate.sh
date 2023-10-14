442b8955c8567d1329d9dde90cdfe5417fd4b755
ak <a.akindele3000@gmail.com>
2023-10-14 05:27:59 +0000

    add linters to devcontainer and fix found errors

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,7 +3,8 @@
 sudo apt-get update
 
 # install package to resolve hostname.localhost addresses
-sudo apt-get install libnss-myhostname
+# sudo apt-get install libnss-myhostname
+echo "127.0.0.1 k3d-registry.localhost" | sudo tee -a /etc/hosts
 
 # install k3d
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/v5.6.0/install.sh | AG=v5.6.0 bash
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/v5.6.0/install.sh | TAG=v5.6.0 bash
